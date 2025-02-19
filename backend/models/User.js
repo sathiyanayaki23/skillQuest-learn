1e2ff972d5a304872c904960b49a3b2dbc9ed385
sathiyanayaki23 <gsathiyanayaki232@gmail.com>
2025-02-19 22:34:29 +0530

    Updated User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -9,5 +10,17 @@ const userSchema = new mongoose.Schema({
     level: { type: Number, default: 1 },
     createdAt: { type: Date, default: Date.now }
 });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
